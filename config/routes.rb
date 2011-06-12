65de05323949ce0a5c5881b3a425d14fb5659ecc
wizdear <wizdear@naver.com>
2011-06-12 16:49:59 +0900

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   #get "pages/home"
   #get "pages/contact"
   #get "pages/about"
@@ -8,6 +10,7 @@ SampleApp::Application.routes.draw do
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
 
+  match '/signup', :to => 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
