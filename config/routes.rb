ccdc28d006d0805ccafe3f647b8868b2e60defc7
wizdear <wizdear@naver.com>
2011-06-13 21:05:28 +0900

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
+  
+  #get "users/new"
 
   #get "pages/home"
   #get "pages/contact"
   #get "pages/about"
   
   #The custom routs
+  match '/signup', :to => 'users#new'
+  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
 
-  match '/signup', :to => 'users#new'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
