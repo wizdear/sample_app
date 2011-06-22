ce3a9d0e136e4700c02458228013887adfee7b7a
wizdear <wizdear@naver.com>
2011-06-22 15:29:37 +0900

    Done with user edit/update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -9,10 +9,12 @@ gem 'rails', '3.0.9'
 
 gem 'sqlite3-ruby', '1.3.3', :require => 'sqlite3'
 gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0.pre2'
 
 group :development do
   gem 'rspec-rails', '2.5.0'
   gem 'annotate-models', '1.0.4'
+  gem 'faker', '0.3.1'
 end
 
 group :test do
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,6 +21,20 @@ module SessionsHelper
     self.current_user = nil
   end
   
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page"
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+  
   private
 
     def user_from_remember_token
@@ -30,4 +44,13 @@ module SessionsHelper
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
     end
+    
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+    
+    def clear_return_to
+      session[:return_to] = nil
+    end
+    
 end
