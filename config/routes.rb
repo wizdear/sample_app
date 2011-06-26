89bf285b498c8afda373974cbb0c6ae8b16021fa
Jo Ethan <joethan@Jo-Ethans-MacBook-Air.local>
2011-06-26 21:38:37 +0900

    Added user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 SampleApp::Application.routes.draw do
   #get "sessions/new"
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :sessions, :only => [:new, :create, :destroy]
   resources :microposts, :only => [:create, :destroy]
+  resources :relationships, :only => [:create, :destroy]
   #get "users/new"
 
   #get "pages/home"
